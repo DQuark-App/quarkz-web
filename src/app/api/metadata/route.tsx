0e995a21d7820fd679cde2432658cf710b9af830
Alan <103020761+cryingraven@users.noreply.github.com>
2023-09-25 21:26:31 +0700

    fix: metadata API store string

--- src/app/api/metadata/route.tsx
+++ src/app/api/metadata/route.tsx
@@ -2,10 +2,10 @@ import { NextRequest, NextResponse } from 'next/server'
 import Storage from '@/service/storage'
 
 export async function POST(request: NextRequest) {
-    const form = (await request.json()) as { data: string; album_uid: string }
-    const buffer = Buffer.from(form.data, 'base64')
-
-    const cid = await Storage.instance.storeBlob(new Blob([buffer]))
+    const data = await request.json()
+    const cid = await Storage.instance.storeBlob(
+        new Blob([JSON.stringify(data)])
+    )
 
     return NextResponse.json({
         cid: cid,
