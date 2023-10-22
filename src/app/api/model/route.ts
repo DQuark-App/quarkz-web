ded369f80bc5548efae5c95580b088c856c6f424
Alan <103020761+cryingraven@users.noreply.github.com>
2023-10-22 22:16:28 +0700

    feat: model order by name

--- src/app/api/model/route.ts
+++ src/app/api/model/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server'
 import SupaBaseService from '@/service/supabase'
 
 export async function GET(request: NextRequest) {
-    const result = await SupaBaseService.instance.from('model').select('*')
+    const result = await SupaBaseService.instance.from('model').select('name, image').order('name', {
+        ascending: true,
+    })
     return NextResponse.json({ data: result.data || [] })
 }
